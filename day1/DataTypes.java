337fb27162dfe11c115a5509d1224ae3bc6a0414
Harishmasekar <harishmasrisekar13@gmail.com>
2025-04-26 21:44:20 +0530

    Add files via upload
    Day1 Assignment

---
+++ day1/DataTypes.java
@@ -0,0 +1,38 @@
+package week1.day1;
+
+public class DataTypes {
+	public static void main(String[] args) {
+		//DataTypes
+		byte age=32;
+		System.out.println("age");
+		System.out.println(age);
+		
+		int noOfStudents=123456789;
+		System.out.println(noOfStudents);
+		
+		// to create a long the value should consist suffix 'L' at last
+		long phNo=123456789L;
+		System.out.println(phNo);
+		
+		short regNo=12345;
+		System.out.println(regNo);
+		
+		// to create a float the value should consist suffix 'f' at last
+		float speed=3.65712973567f;
+		System.out.println(speed);
+		
+		double speed1=3.65712973567;
+		System.out.println(speed1);
+		
+		char logo='s';
+		System.out.println(logo);
+		 
+		boolean isvisible=true;
+		System.out.println(isvisible);
+		
+		
+		String name="Harishma";
+		System.out.println(name);
+		
+	}
+}
---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,9 @@
+package week1.day1;
+
+public class HelloTestLeaf {
+
+	public static void main(String[] args) {
+		// TODO Auto-Generated Method Stud
+		System.out.println("Hi All Welcome to Test leaf");
+	}
+}
