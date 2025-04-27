25641f88d9910d0e40b9ed7ba43718056923694c
Harishmasekar <harishmasrisekar13@gmail.com>
2025-04-27 18:08:50 +0530

    Add files via upload
    Week1 Day2

---
+++ day2/FindOddNumbers.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class FindOddNumbers {
+
+	  public static void main(String[]args) {
+		  for (int maxRange =  0; maxRange <=10; maxRange++) {
+			  if (maxRange % 2==1) {
+				  System.out.println("The odd number is " +maxRange);
+	            }
+	        }
+	    }
+
+	}
+
+
