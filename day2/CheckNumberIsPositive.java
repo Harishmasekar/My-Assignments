6414911c6a5af3ddad66aa59d4f57dec692ec687
Harishmasekar <harishmasrisekar13@gmail.com>
2025-05-02 12:42:57 +0530

    Add files via upload
    Week1 day2

---
+++ day2/CheckNumberIsPositive.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class CheckNumberIsPositive {
+	public static void main(String[] args) {
+		int num=-2;
+		
+		if (num>=0) {
+			System.out.println("The given number is Positive"+" "+num);
+		} 
+		else 
+		{
+             System.out.println("The given number is Negative"+" "+num);
+		}
+	}
+
+}
--- day2/FindOddNumbers.java
+++ day2/FindOddNumbers.java
@@ -2,14 +2,11 @@ package week1.day2;
 
 public class FindOddNumbers {
 
-	  public static void main(String[]args) {
+	 public static void main(String[]args) {
 		  for (int maxRange =  0; maxRange <=10; maxRange++) {
 			  if (maxRange % 2==1) {
 				  System.out.println("The odd number is " +maxRange);
 	            }
 	        }
 	    }
-
-	}
-
-
+}
